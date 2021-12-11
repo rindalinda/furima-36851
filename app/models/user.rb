7f7143637666fd4291daeac625d102211d064b2d
rindalinda <lindarinda0227@icloud.com>
2021-12-11 12:08:00 +0900

    バリデーションの追加実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname    , presence: true
-  validates :last_name1  , presence: true
-  validates :first_name1 , presence: true
-  validates :last_name2  , presence: true
-  validates :first_name2 , presence: true
+    validates :nickname    , presence: true
+    validates :encrypted_password, format: { with: /[a-z\d]{6,}/i }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name1
+    validates :first_name1
+  end
+  with_options presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ } do
+    validates :last_name2
+    validates :first_name2
+  end
   validates :birthday    , presence: true
 
-  
+
 end
