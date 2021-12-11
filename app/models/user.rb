d54f08697c4515033011f0c1f2fce54957295cd5
rindalinda <lindarinda0227@icloud.com>
2021-12-11 17:24:34 +0900

    rubocop実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname    , presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters'} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' } do
     validates :last_name1
     validates :first_name1
   end
 
-  with_options presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: 'Input full-width katakana characters'} do
+  with_options presence: true,
+               format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/,
+                         message: 'Input full-width katakana characters' } do
     validates :last_name2
     validates :first_name2
   end
 
-  validates :birthday    , presence: true
-
-
+  validates :birthday, presence: true
 end
