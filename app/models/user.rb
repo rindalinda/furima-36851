e9dced2d9e3903d0f1ef51bf433dc787f3fef6a9
rindalinda <lindarinda0227@icloud.com>
2021-12-11 17:17:00 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    validates :nickname    , presence: true
-    validates :encrypted_password, format: { with: /[a-z\d]{6,}/i }
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+  validates :nickname    , presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters'} do
     validates :last_name1
     validates :first_name1
   end
-  with_options presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ } do
+
+  with_options presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: 'Input full-width katakana characters'} do
     validates :last_name2
     validates :first_name2
   end
+
   validates :birthday    , presence: true
 
 
