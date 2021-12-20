35e5426efb02e614fcc897af99672396f49b4fd9
rindalinda <lindarinda0227@icloud.com>
2021-12-20 12:27:27 +0900

    rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :explain, :category_id, :status_id, :charge_id, :prefecture_id, :send_day_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :explain, :category_id, :status_id, :charge_id, :prefecture_id, :send_day_id,
+                                 :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :send_day
 
-  validates :image,:item_name,:explain,:category,:status, :charge, :prefecture, :send_day, :price, presence: true
+  validates :image, :item_name, :explain, :category, :status, :charge, :prefecture, :send_day, :price, presence: true
   with_options numericality: { other_than: 1 } do
-  validates :category_id, numericality: { other_than: 1 }
-  validates :status_id, numericality: { other_than: 1 }
-  validates :charge_id, numericality: { other_than: 1 }
-  validates :send_day_id, numericality: { other_than: 1 }
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :charge_id, numericality: { other_than: 1 }
+    validates :send_day_id, numericality: { other_than: 1 }
   end
   validates :prefecture_id, numericality: { other_than: 0 }
   validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
