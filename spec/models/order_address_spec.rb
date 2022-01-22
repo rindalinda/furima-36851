ed03267cbcba5ea170d9ff946547f5a2eb848035
rindalinda <lindarinda0227@icloud.com>
2022-01-22 11:16:15 +0900

    最終修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe OrderAddress, type: :model do
       end
 
       it '都道府県に「---」が選択されている場合は出品できない' do
-        @order_address.prefecture_id = '0'
+        @order_address.prefecture_id = 0
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Prefecture must be other than 0')
       end
